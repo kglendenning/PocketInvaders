72fa66cc0dc453cf3b41de583d6b31c332307d5e
ChrisBlair <chris.blair777@yahoo.com>
2013-12-15 22:50:55 -0500

    exclude

--- GameApp/src/gameapp/Player.java
+++ GameApp/src/gameapp/Player.java
@@ -153,18 +153,18 @@ public class Player extends Ship {
         int ypoints[] = {getY() + getHeight(), getY() + getHeight(), getY()};
         return new Polygon(xpoints, ypoints, 3).intersects(powerup.getBoundingBox());
     }
-    
+
     /**
      * @param damage Damage of projectile
      * @return 0 - if nothing, 1 - if killed
      */
     @Override
-    public int takeDamage(int damage){
-        setHealth(getHealth()-damage);
-        if(getHealth() < 0){
+    public int takeDamage(int damage) {
+        setHealth(getHealth() - damage);
+        if (getHealth() < 0) {
             setHealth(0);
         }
-        
+
         return getHealth() > 0 ? 0 : 1;
     }
 
@@ -183,10 +183,10 @@ public class Player extends Ship {
     }
 
     public void collect(int type) {
-        if(weaponAmmo[type] <= 0){
+        if (weaponAmmo[type] <= 0) {
             setActiveWeapon(type);
         }
-        
+
         weaponAmmo[type] += weapon.getReload(type);
         if (weaponAmmo[type] > weapon.getMax(type)) {
             weaponAmmo[type] = weapon.getMax(type);
