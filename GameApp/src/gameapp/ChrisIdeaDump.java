cddd2cae123d95d581de159b7a619b3cb0df4c37
ChrisBlair <chris.blair777@yahoo.com>
2013-12-23 18:27:16 -0500

    Revised my ideas again...OCD

--- GameApp/src/gameapp/ChrisIdeaDump.java
+++ GameApp/src/gameapp/ChrisIdeaDump.java
@@ -3,10 +3,9 @@ package gameapp;public class ChrisIdeaDump{}
 /*
 
 Below is a hierarchy....chart....thing...to get my ideas flowing; a revision of the current design if we were making the game 
-"directionless" aka not biased to the forward direction, and more robust
+"directionless" aka not biased to the forward direction, and more robust in general
 
-Entity: name, x, y, movement_style, movement_speed, angle, 
-        model (one crazy-ass class that handles the image and hitbox? we should discuss the possibility of deriving hitbox from sprite)
+Entity: name, x, y, movement_style, movement_speed, angle, model (one class that handles the image and hitbox? we should discuss the possibility of deriving hitbox from image)
     Sidebar: sidebar_image (for all sidebar-specific graphics; an interface)
         Powerup: drop_rate, drop_chart, effect
         Weapon: ammo, ammo_max, fire_rate
@@ -17,7 +16,7 @@ Entity: name, x, y, movement_style, movement_speed, angle,
     Effect: duration
         *Effects*
     Ship: health, health_max, weapons, weapon_current, buffs, buff_current, fire_rate_factor, affinity, invuln,  
-          offense_factor, defense_factor, collision_damage, collision_detect, regen_health, regen_ammo,
+          offense_factor, defense_factor, collision_damage, collision_detect, health_regen, ammo_regen
         Player
         *Enemies*
 
